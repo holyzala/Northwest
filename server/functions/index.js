d140bc6fbbb96a08204563fd81fd100da8e229d0
Christopher Magnuson <magnus32@uwm.edu>
2018-04-03 16:39:50 -0500

    Add express, do a super simple route

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,4 +1,8 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {res.json("Hello");});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +10,5 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.app = functions.https.onRequest(app);
